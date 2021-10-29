667e98b6ccebfcb07eb0a6fec238660a5d1d43f7
Roman Klym <klymroman@protonmail.com>
2021-10-29 14:47:04 +0300

    added variables options

--- speech_recog.py
+++ speech_recog.py
@@ -9,59 +9,72 @@ import wavio
 from playsound import playsound
 from threading import Thread
 
+PATH = 'E:\Code\speech100'
+
+stt_lang = "uk-UA"
+tts_lang = 'en'
+from_lang = 'ukrainian'
+to_lang = 'english'
+accent = 'ca'
+
+
 def warning():
-    tts = gTTS(text='I sincerely apologize for bad translation ahead, if someone is to be blamed its Google AI department. Enjoy', tld='ca', lang='en', slow=False)
+    tts = gTTS(text='I sincerely apologize for bad translation ahead, if someone is to be blamed its Google AI department. Enjoy',
+               tld=accent, lang=tts_lang, slow=False)
 
     tts.save('sorry.mp3')
 
-    playsound('E:\Code\speech100\sorry.mp3')
-    
+    playsound('{}\sorry.mp3'.format(PATH))
+
     return None
 
 
 def speech_to_speech(number=0):
     start_time = time.time()
 
-    fs = 44100 
+    fs = 44100
     seconds = 30
 
     print("Recording...")
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2, dtype='float64')
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs,
+                         channels=2, dtype='float64')
     sd.wait()
+
     print("Saving!")
-    wavio.write('preaching{}.wav'.format(number), myrecording, fs ,sampwidth=2)
+    wavio.write('preaching{}.wav'.format(number), myrecording, fs, sampwidth=2)
 
     print("Recording takes about: {} seconds".format(time.time() - start_time))
 
     r = sr.Recognizer()
 
     preaching = sr.AudioFile(
-        'E:\Code\speech100\preaching{}.wav'.format(number))
+        '{}\preaching{}.wav'.format(PATH, number))
     with preaching as source:
         audio = r.record(source)
 
-    to_translate = r.recognize_google(audio, language="uk-UA")
+    to_translate = r.recognize_google(audio, language=stt_lang)
     print(to_translate)
 
     translated = GoogleTranslator(
-        source='ukrainian', target='english').translate(to_translate)
+        source=from_lang, target=to_lang).translate(to_translate)
     print(translated)
 
-    tts = gTTS(text=translated, tld='ca', lang='en', slow=True)
+    tts = gTTS(text=translated, tld=accent, lang=tts_lang, slow=True)
 
     tts.save('hello{}.mp3'.format(number))
 
-    playsound('E:\Code\speech100\hello{}.mp3'.format(number))
+    playsound('{}\hello{}.mp3'.format(PATH, number))
 
     return None
 
 
 def main():
-    
+
     warning()
-    
+
     n = 1
-    while True and n <= 10:
+    while True and n <= 180:
         start_time = time.time()
 
         t1 = Thread(target=speech_to_speech, args=(n,))
